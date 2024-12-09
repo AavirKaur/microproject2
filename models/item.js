7ac083581b31eb9448f4a3f89697374f3e670a74
AavirKaur <136387416+AavirKaur@users.noreply.github.com>
2024-12-08 23:12:59 -0500

    Create item.js

---
+++ models/item.js
@@ -0,0 +1,12 @@
+// models/item.js
+
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true }
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+export default Item;
